33ef0f94bba6f36707e62328290482a8c12131e7
Isaac Harvey <ijharvey456@gmail.com>
2020-11-28 17:52:49 -0600

    Set up gpt-2-simple example

--- predictor.py
+++
@@ -1,6 +0,0 @@
-# predictor.py
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
-# download the pretrained DistilGPT2 model and set it to evaluation
-model = GPT2LMHeadModel.from_pretrained("distilgpt2")
-model.eval()
