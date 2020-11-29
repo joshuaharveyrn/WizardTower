fe8911a6091167445269351a1f1f12b6ad4223c7
Isaac Harvey <ijharvey456@gmail.com>
2020-11-29 13:17:36 -0600

    Uses training_data.txt now

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import gpt_2_simple as gpt2
 import os
 import requests
+import sys
 
 model_name = "124M"
+
 if not os.path.isdir(os.path.join("models", model_name)):
 	print(f"Downloading {model_name} model...")
 	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
+file_name = "training_data.txt"
 
-file_name = "shakespeare.txt"
 if not os.path.isfile(file_name):
-	url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
-	data = requests.get(url)
-	
 	with open(file_name, 'w') as f:
-		f.write(data.text)
-    
+		f.write("Put stuff in here for training please.")
+		sys.exit("You need to have the training data file ready before you execute the program.")
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
